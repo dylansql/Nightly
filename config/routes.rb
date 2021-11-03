12069fb8837dd0b63e0be98654131802420e864c
koji <koji@kojis-MBP.home>
2021-11-02 20:18:55 -0700

    added seed data not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
   resources :users
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+@admin = User.create!(username: 'tylerWash', email: 'tylergotacrush@ga.com', password: '123456')
+
+puts "#{User.count} users created"
+
+3.times do
+  Post.create!(
+    title: Faker::FunnyName,
+    categorey: 'Sleepless Nights',
+    image: 'https://picsum.photos/200',
+    content: Faker::Quote,
+    user: @admin
+  )
+end
+
+3.times do
+  Post.create!(
+    title: Faker::FunnyName,
+    categorey: 'Sleep Advice',
+    image: 'https://picsum.photos/200',
+    content: Faker::Quote,
+    user: @admin
+  )
+end
+
+3.times do
+  Post.create!(
+    title: Faker::FunnyName,
+    categorey: 'Somethings on My Mind',
+    image: 'https://picsum.photos/200',
+    content: Faker::Quote,
+    user: @admin
+  )
+end
+
+puts "#{Post.count} users created"
+
+@good = Comment.create!(content: 'This is great information')
+
+@bad = Comment.create!(content: 'This was not great')
+
+@soso = Comment.create!(content: 'This was decent')
+
+Post.all.each do |post|
+  rand(1..3).times do
+    post.comments << Comment.all.sample
+  end
+end
