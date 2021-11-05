e031ca9ac7aae3bae430d93a4702c44e66e728e7
koji <dylanbogerr@outlook.com>
2021-11-05 07:36:37 -0700

    Working on listing page need to do category sort

--- client/src/screens/Listing/Listing.jsx
+++ client/src/screens/Listing/Listing.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState }  from 'react'
 
 export default function Listing({posts}) {
+    const [category, setCategory ] = useState("Sleepless Nights")
+    const [categoryTwo, setCategoryTwo ] = useState("Sleep Advice")
+    const [categoryThree, setCategoryThree ] = useState("Somethings on My Mind")
 
     return (
         <div>
             <h1>This is listing/Posts</h1>
-            <div className="button-selections">
-                <button>Sleep Nights?</button>
-                <button>Sleep Advice?</button>
-                <button>Something on Your Mind?</button>
-            </div>
+            <button onClick={() => setCategory("Sleepless Nights")}>
+            {category}
+            
+            </button>
+            <button onClick={() => setCategoryTwo("Sleep Advice")}>
+            {categoryTwo}
+            </button>
+            <button onClick={() => setCategoryThree("Somethings on My Mind")}>
+            {categoryThree}
+            </button>
+            <div>
             {posts.map((post) => (
-        <p key={post.id}>{post.title}</p>
+                <div className="result-box" key={post.id} >
+                <Link to={`/posts/${post.id}`}>
+                <img src={post.image} />
+                <p key={post.id}>{post.title}</p>
+                </Link>
+                </div>
       ))}
+      </div>
 
         </div>
     )
