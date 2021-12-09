eeb1f63e878a208d29f8df1eb40e87d7ac5128b7
koji <dylanbogerr@outlook.com>
2021-12-09 10:20:51 -0500

    front end configs

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://nightly-api.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
-});
-
+})
 export default api;
