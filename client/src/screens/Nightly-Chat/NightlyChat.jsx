801d6002eddeca125e51d8db655b62c0ff241d78
koji <dylanbogerr@outlook.com>
2022-01-26 16:23:19 -0500

    commiting before backend push

--- client/src/screens/Nightly-Chat/NightlyChat.jsx
+++ client/src/screens/Nightly-Chat/NightlyChat.jsx
@@ -1,10 +1,9 @@
-import './NightlyChat.css'
+import './nightlycss.css'
 import { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 
 const socket = io("http://localhost:3002")
-
 
 export default function NightlyChat(props) {
 
@@ -19,15 +18,15 @@ const [user, setUser] = useState("")
 const sendChat = (e) => {
     e.preventDefault()
     socket.emit('chat-message', {nickname: nickname, msg: msg, roomTag: tag})
-    console.log(tag)
     setMessage("")
 }
+
 useEffect(() => {
     socket.on("chat-message", (payload) => {
         setNickname(currentUser?.username)
         setChat([...chat, payload])
     })
-});
+}, [msg]);
 
 
 return (
@@ -40,18 +39,18 @@ return (
                     {chat.map((payload, index) => {
                         return(
                             <div className="chat-li">
-                            <p key={index}>{payload}</p>
+                                <p key={index}>{payload}</p>
                             </div>
                         )
                     })}
-                    <div className="chat-input">
-                    <form onSubmit={sendChat}>
-                        <input type="text" name="chat" placeholder="Type Something..." onChange={(e) => {
-                            setMessage(e.target.value) 
-                        }}/>
-                        <button type="submit">Send</button>
-                    </form>
                     </div>
+                    <div className="chat-input">
+                        <form onSubmit={sendChat}>
+                            <input id="group-chat-box" type="text" name="chat" placeholder="Type Something..." onChange={(e) => {
+                                setMessage(e.target.value) 
+                            }}/>
+                            <button type="submit">Send</button>
+                        </form>
                 </div>
             </div>
         </div>
