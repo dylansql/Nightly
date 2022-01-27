5b3da6a133e3c64c7ffd617804b89d3decd3f432
koji <dylanbogerr@outlook.com>
2022-01-27 17:07:40 -0500

    deployment changes

--- client/src/screens/Nightly-Chat/NightlyChat.jsx
+++ client/src/screens/Nightly-Chat/NightlyChat.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3002
 
-const socket = io(PORT, {
+const socket = io([PORT], {
     withCredentials: true,
     extraHeaders: {
     "nightly": "true"
