e09b4fb21b99f43b480decdfb3e460eb8a4785b3
koji <dylanbogerr@outlook.com>
2022-01-27 01:08:26 -0500

    removed connsole log in server for test

--- nightly-chat-server/index.js
+++ nightly-chat-server/index.js
@@ -9,13 +9,11 @@ const PORT = 3002 || process.env.PORT
 const httpServer = createServer();
 const io = new Server(httpServer, { 
     cors: {
-        origin: ["https://nightly-chat-server.herokuapp.com/", "https://nightlyy.netlify.app/"],
+        origin: ["https://nightly-chat-server.herokuapp.com/", "https://nightlyy.netlify.app/"]
       },
-    credentials: true,
-    path:'https://nightly-chat-server.herokuapp.com/'
+    path:'https://nightly-chat-server.herokuapp.com/',
+    credentials: true
  });
-
-console.log(io)
 
  app.get('/', (req, res) => {
      res.sendFile(__dirname + '/index.html');
