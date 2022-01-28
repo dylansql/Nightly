021681b8c7b794187c5c0d9780ae63833205abb7
koji <dylanbogerr@outlook.com>
2022-01-27 22:41:58 -0500

    new push changed local host to heroku url

--- client/src/screens/Nightly-Chat/NightlyChat.jsx
+++ client/src/screens/Nightly-Chat/NightlyChat.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 
-const backend = "http://localhost:3002" || "https://nightly-chat-server.herokuapp.com/";
+const backend = "https://nightly-chat-server.herokuapp.com/";
 
 const socket = io(backend, {
     withCredentials: true,
